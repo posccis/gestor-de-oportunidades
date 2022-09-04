103d0a834111ea0c85d9706f1746c398a2d23457
Posccis <lkzq33@gmail.com>
2022-09-04 13:01:08 -0300

    new(main): Criada a documentação com o swagger

--- GerenciamentoDeOportunidades/GerenciamenetoDeOportunidadesAPI/Controllers/GerenciamentoDeOportunidadesController.cs
+++ GerenciamentoDeOportunidades/GerenciamenetoDeOportunidadesAPI/Controllers/GerenciamentoDeOportunidadesController.cs
@@ -9,6 +9,22 @@ namespace GerenciamenetoDeOportunidadesAPI
         Manutencao manu = new Manutencao();
         ExceptionsTratamento ex = new ExceptionsTratamento();
         #region POST
+        /// <summary>
+        /// Cadastra um usuário(vendedor) no sistema de Gerenciamento.
+        /// </summary>
+        /// <param name="usuario"></param>
+        /// <returns code="201">Confirmando a criação.</returns>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /Todo
+        ///     {
+        ///        "emailId": "xxxx@xxx.com",
+        ///        "nome": "xxxxxx",
+        ///        "regiao": 1
+        ///     }
+        ///
+        /// </remarks>
         [AcceptVerbs("POST"), Route("api/usuario")]
         [HttpPost]
         public IActionResult EnviarUsuario([FromBody]Usuario usuario) 
@@ -16,7 +32,7 @@ namespace GerenciamenetoDeOportunidadesAPI
             try
             {
                 ex.ValidarUsuarioInsercao(usuario);
-                return manu.EnviarUsuario(usuario)  ? Ok() : BadRequest() ;
+                return manu.EnviarUsuario(usuario)  ? StatusCode(StatusCodes.Status201Created, usuario) : BadRequest() ;
             }
             catch (GerenciamentoDeOportunidadesException)
             {
@@ -30,7 +46,22 @@ namespace GerenciamenetoDeOportunidadesAPI
         }
 
 
-
+        /// <summary>
+        /// Cadastra uma oportunidade no sistema de gerenciamento. 
+        /// </summary>
+        /// <param name="opo"></param>
+        /// <returns code="201">Confirmando a criação.</returns>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /Todo
+        ///     {
+        ///        "nome": "xxxxxx",
+        ///        "valormonetario": 1,
+        ///        "cnpj": "00.000.000/0000-00"
+        ///     }
+        ///
+        /// </remarks>
         [AcceptVerbs("POST"), Route("api/oportunidade")]
         [HttpPost]
         public IActionResult EnviarOportunidade([FromBody] Oportunidade opo)
@@ -50,7 +81,11 @@ namespace GerenciamenetoDeOportunidadesAPI
         #endregion POST
 
         #region GET
-
+        /// <summary>
+        /// Retorna um vendedor com suas oportunidades. Necessário informar o email.
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns code="200">Retorna o objeto completo.</returns>
         [AcceptVerbs("GET"), Route("api/usuario/{email}")]
         [HttpGet]
         public ActionResult<Usuario> ObterUsuario(string email)
--- GerenciamentoDeOportunidades/GerenciamenetoDeOportunidadesAPI/Program.cs
+++ GerenciamentoDeOportunidades/GerenciamenetoDeOportunidadesAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +8,30 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Gerenciamento de Oportunidades API",
+        Description = "Uma API para gerenciar as oportunidades de venda e os vendedores de uma organização.",
+        TermsOfService = new Uri("https://developers.google.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "GitHub",
+            Url = new Uri("https://github.com/posccis")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Licença",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
 var app = builder.Build();
 
@@ -15,6 +41,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 
